c265c465634c213b471e3b141d0b4c4f1e59a9e1
Drew Koch <dkoch13@gmail.com>
2014-04-04 10:13:02 -0700

    redirect search to expeditions page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 $('#mobile-menu-trigger').on('click', function() {
   $('#mobile-menu').addClass("visible").addClass("animated").toggleClass("slideInDown").toggleClass("slideOutUp");
 });
+
+$('#hero-form').submit(function() {
+  window.location = 'expeditions.html';
+  event.preventDefault();
+})
